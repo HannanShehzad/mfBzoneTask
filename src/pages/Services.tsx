116ab881387195d326389cddb2cbddd7b2d66988
Hannan <hannan.shehzad201999@gmail.com>
2023-07-29 02:26:56 +0500

    ..

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -7,74 +7,61 @@ import Query from '../components/Query'
 import '../components/Styles/services.css'
 
 const Services = () => {
-  return (
+  const servisesSard = [
+    { imageSrc: './images/image1.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image2.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image3.jpeg', heading: 'Noteworthy technology acquisitions 2021' },
     
+   
+  ]
+  const tradeCard = [
+    { imageSrc: './images/image4.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image5.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image6.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image7.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image8.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image9.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image10.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image11.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image12.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image13.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image14.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image15.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image16.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image17.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image18.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image19.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image20.png', heading: 'Noteworthy technology acquisitions 2021' },
+    { imageSrc: './images/image21.png', heading: 'Noteworthy technology acquisitions 2021' },
+  ]
+
+  return (
     <div>
       <TopHeader  />
       <Navbar/>
       <h1>Services</h1>
-      <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image1.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image2.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image3.jpeg" heading="Noteworthy technology acquisitions 2021" />  
+      <div className="flex justify-center "> 
+      <div className=' flex flex-row flex-wrap items-center'>
+        {servisesSard.map(({ imageSrc, heading }, index) => {
+          return <Card key={index} imageSrc={imageSrc} heading={heading} /> 
+        })}
       </div>
-    </div>
-    <h1>Our Trades</h1>
-    <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image4.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image5.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image6.png" heading="Noteworthy technology acquisitions 2021" />         
-         
-      </div>
-    </div>
-    <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image7.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image8.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image9.png" heading="Noteworthy technology acquisitions 2021" />         
-         
-      </div>
-    </div>
-    <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image10.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image11.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image12.png" heading="Noteworthy technology acquisitions 2021" />         
-         
-      </div>
-    </div>
-    <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image13.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image14.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image15.png" heading="Noteworthy technology acquisitions 2021" />         
-         
-      </div>
-    </div>
-    <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image16.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image17.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image18.png" heading="Noteworthy technology acquisitions 2021" />         
-         
-      </div>
-    </div>
-    <div className="flex justify-center"> 
-      <div className='sMargin flex flex-row flex-wrap items-center'>
-        <Card imageSrc="./images/image19.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image20.png" heading="Noteworthy technology acquisitions 2021" />  
-        <Card imageSrc="./images/image21.png" heading="Noteworthy technology acquisitions 2021" />         
-         
-      </div>
-    </div>
-
-      <Query />
-
-      <Footer />
       
-        </div>
+    </div>
+
+    <h1>Our Trades</h1>
+      <div className="flex justify-center TAlign"> 
+      <div className=' flex flex-row flex-wrap items-center'>
+        {tradeCard.map(({ imageSrc, heading }, index) => {
+          return <Card key={index} imageSrc={imageSrc} heading={heading} /> 
+        })}
+      </div>
+      
+    </div>
+
+    <Query />
+    <Footer />
+    </div>
   )
 }
 
